575d435c5ada3ef89df1d977480476248b3e42eb
treva5 <trevamukisa5@gamil.com>
2025-06-22 16:34:10 +0300

    branch 6:second commit

--- lossamount.c
+++ lossamount.c
@@ -7,18 +7,22 @@ int main() {
     printf("Enter the cost price of the item in UGX: ");
     scanf("%f", &cost_price);
 
+    // Input selling price
+    printf("Enter the selling price of the item in UGX: ");
+    scanf("%f", &selling_price);
 
     // Calculate the difference
     difference = selling_price - cost_price;
 
     // Check if profit or loss
     if (difference > 0) {
-        printf("Profit amount: UGX %.2fn", difference);
+        printf("Profit amount: UGX %.2f\n", difference);
     } else if (difference < 0) {
-
+        printf("Loss amount: UGX %.2f\n", -difference);
     } else {
         printf("No profit, no loss.\n");
     }
+    system("pause");
 
     return 0;
 }
