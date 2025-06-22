872c301e8cd676d974f4f0e108451c419897f555
treva5 <trevamukisa5@gamil.com>
2025-06-22 16:45:24 +0300

    branch 7:second commit

--- netamount.c
+++ netamount.c
@@ -5,11 +5,18 @@ int main() {
 
     // Input withdrawal amount
     printf("Enter the withdrawal amount in UGX: ");
-    scanf("%f", t);
+    scanf("%f", &withdrawal_amount);
 
+    // Calculate net amount after fee
+    if (withdrawal_amount < 500) {
+        net_amount = withdrawal_amount - 10; // Deduct transaction fee
+    } else {
+        net_amount = withdrawal_amount; // No fee for amounts 500 and above
+    }
 
     // Display the net amount received
     printf("Net amount received: UGX %.2f\n", net_amount);
+    system("pause");
 
     return 0;
 }
