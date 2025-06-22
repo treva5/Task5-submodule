173a3b8aa8d36328688288d200afbcd2c613a8f5
treva5 <trevamukisa5@gamil.com>
2025-06-22 15:44:43 +0300

    branch 4:first commit

---
+++ salestax.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float purchase_amount, tax_rate, totalamount;
+
+    // Input purchase amount
+    priurchase amount in UGX: ")
+    scanf("%f", &purchase_amount);
+
+    // Determine tax rate based on purchase amount
+    if (purchase_amount < 500) {
+        tax_rate = 0.05;
+    } else         tax_rate = 0.08;
+    }
+
+
+
+    // Display the total amount payable
+    printf("Total payable amount: UGX %.2f\n", total_amount);
+
+    return 0;
+}
