e88689e5285e8094a4c29596b6b9f4a474ae705d
treva5 <trevamukisa5@gamil.com>
2025-06-22 17:23:22 +0300

    branch 10:second commit

--- storeoffers.c
+++ storeoffers.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    float prie_per_unit, total_cost, discount, discounted_price;
+    float price_per_unit, total_cost, discount, discounted_price;
     int quantity;
 
     // Input price per unit
-    printf("Enter the price pr unit in UGX: ");
+    printf("Enter the price per unit in UGX: ");
     scanf("%f", &price_per_unit);
 
     // Input quantity
     printf("Enter the quantity: ");
-    scanf("%d, &quantity);
+    scanf("%d", &quantity);
 
     // Calculate total cost before discount
-    total_cot = price_per_unit * quantity;
+    total_cost = price_per_unit * quantity;
 
     // Apply discount if quantity is 10 or more
     if (quantity >= 10) {
-        disnt = 0.15; // 15% discount
+        discount = 0.15; // 15% discount
         discounted_price = total_cost - (total_cost * discount);
     } else {
         discounted_price = total_cost; // No discount
     }
 
     // Display the total cost after discount
-    printf("Total cost: UGX %.f\n", discounted_price);
+    printf("Total cost: UGX %.2f\n", discounted_price);
+    system("pause");
 
     return 0;
 }
