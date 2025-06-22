b7c0add6fae2a94761dd01b66924319b66a5fc10
treva5 <trevamukisa5@gamil.com>
2025-06-22 17:08:27 +0300

    branch 9:second commit

--- gym.c
+++ gym.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 
-int main) {
-    int membership_years
+int main() {
+    int membership_years;
     float total_fee, discount, final_amount;
 
     // Input membership duration
     printf("Enter membership duration in years: ");
-    scanf("%d", &membershipyears);
+    scanf("%d", &membership_years);
 
     // Input total fee
     printf("Enter the total fee: UGX ");
     scanf("%f", &total_fee);
 
     // Determine discount based on membership duration
-    if (membershp_years >= 1 && membership_years <= 3) {
+    if (membership_years >= 1 && membership_years <= 3) {
         discount = 0.05; // 5% discount
-    } else if (meship_years > 3) {
+    } else if (membership_years > 3) {
         discount = 0.10; // 10% discount
     } else {
         discount = 0; // No discount for less than 1 year
     }
 
     // Calculate final amount
-    final_amount = totl_fee - (total_fee * discount);
+    final_amount = total_fee - (total_fee * discount);
 
     // Display final payable amount
-    printf("Fial payable amount: UGX %.2f\n", final_amount);
+    printf("Final payable amount: UGX %.2f\n", final_amount);
+    system("pause");
 
     return 0;
 }
