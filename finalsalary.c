2fff6b6c0030e1c82bac583590a0196811a084b3
treva5 <trevamukisa5@gamil.com>
2025-06-22 15:04:54 +0300

    branch 2: first commit

---
+++ finalsalary.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float basic_salary, final_salary;
+    int years_of_service;
+
+    // Input years of service
+
+    // Calculate final salary with bonus if applicable
+    if (years_of_service > 5) {
+        final_salary = basic_salary + (basic_salary * 0.05); // 5% bonus
+    } else {
+        final_salary = basic_salary;
+    }
+
+    // Display the final salary
+    printf
+
+    return 0;
+}
