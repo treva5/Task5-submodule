1613dac625344e90becc4c97360e2d84d2c50796
treva5 <trevamukisa5@gamil.com>
2025-06-22 14:55:47 +0300

    branch 1: second commit

--- finalamount.c
+++ finalamount.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    float purchase_amount
+    float purchase_amount, final_amount;
 
-    // Prompt user for total pue amount in UGX: ");
+    // Prompt user for total purchase amount
+    printf("Enter the total purchase amount in UGX: ");
     scanf("%f", &purchase_amount);
 
     // Check if amount exceeds 1000 and apply discount
     if (purchase_amount > 1000) {
-        f - (purchase_amount * 0.1); // 10% discount
+        final_amount = purchase_amount - (purchase_amount * 0.1); // 10% discount
     } else {
-        final_amount = purcnt;
+        final_amount = purchase_amount;
     }
 
     // Display the final amount
     printf("Final amount after discount (if applicable): UGX %.2f\n", final_amount);
+    system("pause");
 
     return 0;
 }
