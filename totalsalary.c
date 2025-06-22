48647f5d27521ac0e09667460c5528a29786ad6e
treva5 <trevamukisa5@gamil.com>
2025-06-22 16:07:56 +0300

    branch 5:second commit

--- totalsalary.c
+++ totalsalary.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    float hours_ hourly_rate, total_salary, overtime_pay = 0;
-    const float overtimemultiplier = 1.5;
+    float hours_worked, hourly_rate, total_salary, overtime_pay = 0;
+    const float overtime_multiplier = 1.5;
     const int regular_hours = 40;
 
     // Input hours worked
     printf("Enter hours worked in the week: ");
-    scanf("%f", &hours_worked)
+    scanf("%f", &hours_worked);
+
     // Input hourly rate
     printf("Enter hourly rate in UGX: ");
     scanf("%f", &hourly_rate);
 
     // Calculate salary
     if (hours_worked > regular_hours) {
-
+        // Calculate overtime hours
+        float overtime_hours = hours_worked - regular_hours;
         // Calculate overtime pay
         overtime_pay = overtime_hours * hourly_rate * overtime_multiplier;
         // Calculate total salary
-        total_salary = (regula * hourly_rate) + overtime_pay;
+        total_salary = (regular_hours * hourly_rate) + overtime_pay;
     } else {
         // No overtime
-        total_salary =_worked * hourly_rate;
+        total_salary = hours_worked * hourly_rate;
     }
 
     // Display total salary
     printf("Total salary for the week: UGX %.2f\n", total_salary);
+    system("pause");
 
     return 0;
 }
