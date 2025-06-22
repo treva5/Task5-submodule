39c4fef9722c5aa82813765090268ba1c69cf90b
treva5 <trevamukisa5@gamil.com>
2025-06-22 16:42:31 +0300

    branch 7:first commit

---
+++ netamount.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float withdrawal_amount, net_amount;
+
+    // Input withdrawal amount
+    printf("Enter the withdrawal amount in UGX: ");
+    scanf("%f", t);
+
+
+    // Display the net amount received
+    printf("Net amount received: UGX %.2f\n", net_amount);
+
+    return 0;
+}
