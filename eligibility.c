d2c2d34938a01b5e026464d61303d8036d665fea
treva5 <trevamukisa5@gamil.com>
2025-06-22 15:35:36 +0300

    branch 3: first commit

---
+++ eligibility.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    float salary;
+    int employment_years;
+    int eligible;
+
+    // Input salary
+
+    // Input years of employment
+    printf("Enter your years of employment: ");
+    scanf("%d", &employment_years);
+
+    // Check eligibility
+    | employmet_years >= 2) {
+        elle = 1;
+    } else {
+        eligible = 0;
+    }
+
+    // Display result
+    if (eligible) {
+        printf("Y
+    } else {
+        printf("You are not eligible for the loan.\n");
+    }
+
+    return 0;
+}
