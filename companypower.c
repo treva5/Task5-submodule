3fdd41f5f2816140a4a86e8561628b75331a834d
treva5 <trevamukisa5@gamil.com>
2025-06-22 16:56:02 +0300

    branch 8:second commit

--- companypower.c
+++ companypower.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-int man() {
-    flo units_consumed, total_bill;
+int main() {
+    float units_consumed, total_bill;
 
     // Input units consumed
-    printf("enter the number of units consumed: ");
-    scanf("%f", &uniconsumed);
+    printf("Enter the number of units consumed: ");
+    scanf("%f", &units_consumed);
 
     // Calculate total bill based on usage
-    if (units_cosumed < 200) {
-        total_bill = units_cons * 0.50;
+    if (units_consumed < 200) {
+        total_bill = units_consumed * 0.50;
     } else {
-        total_bill = units_consumed * 5;
+        total_bill = units_consumed * 0.75;
     }
 
     // Display the total bill
-    printf("tal bill: UGX %.2f\n", total_bill);
+    printf("Total bill: UGX %.2f\n", total_bill);
+    system("pause");
 
     return 0;
 }
